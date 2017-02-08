c46e7789de763cb5c02e5540e5cac3763db16c82
Lisa Ma <limawebdev@gmail.com>
2017-02-08 13:38:02 -0700

    end of lesson

---
+++ traversal.js
@@ -0,0 +1,7 @@
+var not1cells = document.getElementsByTagName('td');
+console.log(not1cells);
+var elements = document.querySelectorAll('td');
+var elements2 = document.querySelectorAll('.not1');
+var element3 = document.querySelector('#1');
+
+console.log(elements);
