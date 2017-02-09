51d09da591c2a6ca56b11cf8d70f3ed3c778495f
Lisa Ma <limawebdev@gmail.com>
2017-02-09 01:24:29 -0700

    mad libs demo ready to go

---
+++ events/events.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+  var formButton = document.getElementsByTagName('button')[0];
+  formButton.addEventListener('click', function(){
+    //Hide madlibs-form
+    //Populate the appropriate spans with their corresponding filled out fields
+    //Display madlibs-text
+  })
+  var textButton = document.getElementsByTagName('button')[1];
+  textButton.addEventListener('click', function(){
+    //Hide madlibs-text
+    //Display madlibs-form
+    //Empty out input fields
+  })
+})
