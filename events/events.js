c4b730d762779061cd00179a64ab2854c85507d4
Lisa Ma <limawebdev@gmail.com>
2017-02-09 02:14:15 -0700

    todolist complete

--- events/events.js
+++ events/events.js
@@ -1,14 +1,3 @@
 document.addEventListener('DOMContentLoaded', function(){
-  var formButton = document.getElementsByTagName('button')[0];
-  formButton.addEventListener('click', function(){
-    //Hide madlibs-form
-    //Populate the appropriate spans with their corresponding filled out fields
-    //Display madlibs-text
-  })
-  var textButton = document.getElementsByTagName('button')[1];
-  textButton.addEventListener('click', function(){
-    //Hide madlibs-text
-    //Display madlibs-form
-    //Empty out input fields
-  })
+  
 })
